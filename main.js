82bb78950d3d6b2abce9a3c5c03669c338971df2
Madxmike <mep053@gmail.com>
2022-02-03 05:16:15 -0500

    properly output json for now

--- main.js
+++ main.js
@@ -3,20 +3,17 @@ var inputWindow = document.getElementById("Java_Code");
 
 const generationURL = "http://api.michaelepps.me:8080/parse";
 
-var generateCode = () => 
-{
+var generateCode = () => {
 	var input_code = inputWindow.value;
-	fetch(generationURL, 
-	{
+	fetch(generationURL, {
 		method: 'POST', 
-		mode: 'no-cors',
 		body: input_code
-	}).then((response) => 
-	{
-		outputWindow.value = response;
+	}).then((response) => {
+		return response.json();		
+	}).then((json) => {
+		outputWindow.value = JSON.stringify(json, null, 2);
 		alert("Translation has been completed");
-	}).catch((error) =>
-	{
+	}).catch((error) => {
 		outputWindow.value = error;
 		alert("There was an error in translation");
 	})
