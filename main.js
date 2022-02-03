c55dec496f7ebaaa61056bf90a42b28ce3b2a0e4
Madxmike <mep053@gmail.com>
2022-02-03 02:18:57 -0500

    Cleanup before deploying

---
+++ main.js
@@ -0,0 +1,22 @@
+var outputWindow = document.getElementById("Translate_Code");
+var inputWindow = document.getElementById("Java_Code");
+
+const generationURL = "http://lasik.michaelepps.me:8080/parse";
+
+var generateCode = () => 
+{
+	var input_code = inputWindow.value;
+	fetch(generationURL, 
+	{
+		method: "POST", 
+		body: input_code
+	}).then((response) => 
+	{
+		outputWindow.value = response;
+		alert("Translation has been completed");
+	}).catch((error) =>
+	{
+		outputWindow.value = error;
+		alert("There was an error in translation");
+	})
+}
