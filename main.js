2afbf0595ebe4d9313e16ad6d5808223efdcc8f6
Michael Epps <mep053@gmail.com>
2022-03-03 12:54:24 -0500

    Rework site with new layout and cleanup.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-var outputWindow = document.getElementById("Translate_Code");
-var inputWindow = document.getElementById("Java_Code");
+var inputText = document.getElementById("input-text");
+var outputText = document.getElementById("output-text");
+var inputFile = document.getElementById("input-file");
 
-const generationURL = "http://api.lasik.michaelepps.me:8081/parse";
+var inputEditor = CodeMirror.fromTextArea(inputText, {
+    lineNumbers: true,
+    mode: "text/x-java",
+});
+
+var outputEditor = CodeMirror.fromTextArea(outputText, {
+    lineNumbers: true,
+    mode: "text/x-csharp",
+    readOnly: true,
+});
+
+inputEditor.setSize("100%", "100vh");
+outputEditor.setSize("100%", "100vh")
+
+const generationURL = "https://localhost:7193/parse";
 
 function showSnackbar() {
   var x = document.getElementById("snackbar");
@@ -9,78 +24,65 @@ function showSnackbar() {
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
-var generateCode = () => {	  
-	var input_code = inputWindow.value;
+var translateCode = () => {	  
 	fetch(generationURL, {
 		method: 'POST', 
-		body: input_code
+		body: inputEditor.getValue(),
 	}).then((response) => {
 		return response.text();		
 	}).then((data) => {
-		outputWindow.value = data;
+		outputEditor.setValue(data);
 		alert("Translation has been completed");
 	}).catch((error) => {
-		outputWindow.value = error;
+		outputEditor.setValue(data);
 		alert("There was an error in translation");
 	})
 }
 
-function saveAs_Translate() {
-	if (promptFilename = prompt("Save file as", "")) {
-		var textBlob = new Blob([document.getElementById("Translate_Code").value], {type:'text/plain'});
-		var downloadLink = document.createElement("a");
-		downloadLink.download = promptFilename;
-		downloadLink.innerHTML = "Download File";
-		downloadLink.href = window.URL.createObjectURL(textBlob);
-		downloadLink.click();
-    delete downloadLink;
-    delete textBlob;
-	}
+var loadInputFile = () => {
+    inputFile.click();
 }
 
-function save_JavaCode(){
-    var text = document.getElementById("Java_Code").value;
-    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
-    var blob = new Blob([text], { type: "text/plain"});
-    var anchor = document.createElement("a");
-    anchor.download = "Untitled.Java";
-    anchor.href = window.URL.createObjectURL(blob);
-    anchor.target ="_blank";
-    anchor.style.display = "none"; // just to be safe!
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
+inputFile.onchange = (e) => {
+    var file = e.target.files[0];
+    
+    var reader = new FileReader();
+    reader.readAsText(file,'UTF-8');
+ 
+    reader.onload = readerEvent => {
+       var content = readerEvent.target.result; 
+
+       inputEditor.setValue(content);
+    }
+
+    // Reset the input
+    e.target.value = '';
+}
+
+var saveOutput = () => {
+    var text = outputEditor.getValue();
+    saveTextAsFile(".cs", text);
+}
+
+var saveInput = () => {
+    var text = inputEditor.getValue();
+    saveTextAsFile(".java", text);
  }
 
-function handleFileSelecting(evt) {
-    var files = evt.target.files; // FileList object
+var saveTextAsFile = (fileExtension, text) => {
+    if (promptFilename = prompt(`Save file as *.${fileExtension}`, "")) {
+        text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
 
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e) {
-
-                var content = e.target.result;
-                if (content) {
-                    // Inserting content into textarea, change id if you need
-                    var textarea = document.getElementById('Java_Code');
-                    textarea.innerHTML = content;
-
-                }
-            };
-        })(f);
-
-        reader.readAsText(f);
+        var blob = new Blob([text], { type: "text/plain"});
+    
+        var downloadLink = document.createElement("a");
+        downloadLink.download = promptFilename + fileExtension;
+        downloadLink.innerHTML = "Download File";
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.click();
+    
+        delete downloadLink
+        delete textBlob
     }
-}
-
-function Load() {
-    var filesElem = document.getElementById('files');
-    filesElem.addEventListener('change', handleFileSelecting, false);
-    filesElem.click();
-}
+ }
 
