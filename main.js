00df5617fc94ddcdb1667f6ca7263b0f39ac7a0d
nigelalexanderivery <redblitz29@gmail.com>
2022-03-02 22:20:25 -0500

    Added the save java code button

--- main.js
+++ main.js
@@ -38,6 +38,20 @@ function saveAs_Translate() {
 	}
 }
 
+function save_JavaCode(){
+    var text = document.getElementById("Java_Code").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "Untitled.Java";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+ }
+
 function handleFileSelecting(evt) {
     var files = evt.target.files; // FileList object
 
@@ -69,3 +83,4 @@ function Load() {
     filesElem.addEventListener('change', handleFileSelecting, false);
     filesElem.click();
 }
+
